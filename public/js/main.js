3dc3baf02ea386d50d2f36950e61d7ce06d83e46
Alan Sax <saxal28@gmail.com>
2016-10-05 00:01:09 +0000

    looks really good, effects added

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,27 @@
 // animate with scroll
 $(document).ready(function() {
-    $('#bio, #skills, .card').addClass("hidden").viewportChecker({
+    $('#bio, .card').addClass("hidden").viewportChecker({
         classToAdd: 'visible animated fadeInUp',
         offset: 100
        });
     $('#contact').addClass("hidden").viewportChecker({
-        classToAdd: 'visible animated bounceIn',
+        classToAdd: 'visible animated fadeInLeftBig',
         offset: 100
        });
+    $('#skills-row-left').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated bounceInLeft',
+        offset: 100
+       });
+    $('#skills-row-right').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated bounceInRight',
+        offset: 100
+       });
+});
+
+// button bounce on hover
+$(".btn").hover(
+    function() {
+    $(this).addClass(" animated infinite tada")
+}, function() {
+    $(this).removeClass(" animated infinite tada")
 });
